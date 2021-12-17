259e4fff4d958996736579e00c1f96ae74e2e18f
Darren <thedarrenwatt@gmail.com>
2021-12-17 17:15:18 +0000

    updates yo

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import discord
 import os
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 
 client = discord.Client()
+
+quotes = [
+    "Is this going to be a stand-up fight, Sir, or another bug-hunt?",
+    "Somebody said alien... she thought they said illegal alien and signed up.",
+    "Anytime.  Anywhere.",
+    "How do I get out of this chicken-shit outfit?",
+    "I am ready, man. Ready to get it on.  Check-it-out.  I am the ultimate badass... state of the badass art.  You do not want to fuck with me.",
+    "Independently targetting particle-beam phalanx.  VWAP! Fry half a city with this puppy.",
+    "Don't  worry. Me and my squad of ultimate badasses will protect you. Check-it-out...",
+    "Hey, if you like that, you're gonna love this...",
+    "Hah! Stop your grinnin' and drop your linen! Found 'em.",
+    "You tell me. I only work here."
+]
 
 @client.event
 async def on_ready():
@@ -15,7 +29,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Game over man! Game over!')
+#    if message.content.lower().contains('alien'):
+    if "alien" in message.content.lower():
+        await message.channel.send(random.choice(quotes))
+
 
 client.run(os.getenv('TOKEN'))
