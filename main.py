799a0e65c281dc14d9331436164e8babc46186bd
Darren Watt <thedarrenwatt@gmail.com>
2021-12-18 14:40:23 +0000

    quote updates

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ load_dotenv()
 client = discord.Client()
 
 quotes = [
+    "This floor's freezing.",
+    "Whoooah!  No shit?  I'm impressed.",
     "Is this going to be a stand-up fight, Sir, or another bug-hunt?",
     "Somebody said alien... she thought they said illegal alien and signed up.",
     "Anytime.  Anywhere.",
@@ -17,7 +19,40 @@ quotes = [
     "Don't  worry. Me and my squad of ultimate badasses will protect you. Check-it-out...",
     "Hey, if you like that, you're gonna love this...",
     "Hah! Stop your grinnin' and drop your linen! Found 'em.",
-    "You tell me. I only work here."
+    "You tell me. I only work here.",
+    "He's coming in.  I feel safer already.",
+    "If they're within twenty klicks we'll read it out here, but so far... zip.",
+    "Yeah... but it's a dry heat.",
+    "What're we supposed to use, man? Harsh language?",
+    "Multiple signals. All round. Closing.",
+    "Let's get the fuck out of here!",
+    "We're getting juked! We're gonna die in here!",
+    "Jesus... Jesus... I don't believe it.",
+    "They must be like Gorman. Their signs are real low but they ain't dead!",
+    "Oh, God. Jesus. This ain't happening.",
+    "Look, man, let's just bug out and call it even, okay?",
+    "Maybe you haven't been keeping up on current events, but we just got out asses kicked, pal!",
+    "Well that's great! That's just fucking great, man. Now what the fuck are we supposed to do, man? We're in some real pretty shit now!",
+    "Just tell me what the fuck we're supposed to do now. What're we gonna do now?",
+    "Man, we're not going to make it seventeen hours! Those things are going to come in here, just like they did before, man... they're going to come in here and get us, man, long before...",
+    "Aye-firmative. I'm on it.",
+    "Yeah, right, it runs from the processing station right into the sublevel here.",
+    "Hudson here. A and B sentries are in place and keyed. We're sealing the tunnel.",
+    "They must be wall to wall in there. Look  at those ammo counters go. It's a shooting gallery down there.",
+    "Oh, man.  And I was gettin' short, too! Four more weeks and out. Now I'm gonna buy it on this fuckin' rock. It ain't half fair, man!",
+    "Oh, right! Right! With those things running around. No way.",
+    "Maybe we got 'em demoralized.",
+    "I say we grease this rat-fuck son of a bitch right now!",
+    "Fuuuck! He's dead. (to Burke) You're dogmeat, pal.",
+    "What do you mean, they cut the power? How could they cut the power, man? They're animals.",
+    "It's inside the complex.",
+    "No. No! It ain't you. They're inside. Inside the perimeter. They're in here.",
+    "This signal's weird...must be some interference or something. There's movement all over the place...",
+    "Movement! Signal's clean. Range twenty meters... Seventeen meters..... fifteen meters...",
+    "Twelve meters. Man, this is a big fucking signal. Ten meters.",
+    "Let's go! Let's go!",
+    "Hudson screams as floor panels lift under him, and clawed arms seize him lightning fast, dragging him down..."
+
 ]
 
 @client.event
@@ -29,9 +64,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-#    if message.content.lower().contains('alien'):
-    if "alien" in message.content.lower():
-        await message.channel.send(random.choice(quotes))
+    if client.user.mentioned_in(message):
+        text = "I am Hudson, I respond to the words Alien or Aliens"
+        print(text)
+        await message.channel.send(text)
 
+    if "hicks" in message.content.lower():
+        quote = "Hudson, Sir. He's Hicks."
+        print(quote)
+        await message.channel.send(quote)
+
+    if "alien" in message.content.lower():
+        quote = random.choice(quotes)
+        print(quote)
+        await message.channel.send(quote)
 
 client.run(os.getenv('TOKEN'))
